f74e4a35ce6e6e56cc48a7a0dd2cbfc5941ebaa0
hellomc86 <hellomc86@gmail.com>
2024-02-24 11:43:52 +0500

    first

---
+++ src/schema.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+
+const article = new Schema(
+  {
+    titleOne: String, // String is shorthand for {type: String}
+    titleTwo: String, // String is shorthand for {type: String}
+    genre: String,
+    aticleBody: [
+      {        
+        imageOne: String,
+        subTitle: String,
+        content: String, 
+      }
+    ],    
+    created_date: Date,
+    updated_date: Date,
+  },
+  { versionKey: false }
+);
+module.exports = mongoose.model("articles", article);
